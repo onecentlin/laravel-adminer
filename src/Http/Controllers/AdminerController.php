46a463a027b04a906111fcb3fbdcb567cedb92b6
Winnie Lin <onecentlin@gmail.com>
2020-12-13 12:05:17 +0800

    Fix autologin db driver mappings

--- src/Http/Controllers/AdminerController.php
+++ src/Http/Controllers/AdminerController.php
@@ -24,12 +24,7 @@ class AdminerController extends Controller
     {
         // Autologin
         if (! isset($_GET['db']) && config('adminer.autologin')) {
-            $database_driver = env('DB_CONNECTION');
-            if ($database_driver === "mysql") {
-                $database_driver = "server";
-            }
-
-            $_POST['auth']['driver'] = $database_driver;
+            $_POST['auth']['driver'] = $this->getDatabaseDriver(env('DB_CONNECTION'));
             $_POST['auth']['server'] = env('DB_HOST');
             $_POST['auth']['db'] = env('DB_DATABASE');
             $_POST['auth']['username'] = env('DB_USERNAME');
@@ -52,5 +47,42 @@ class AdminerController extends Controller
     private function getAdminerFileName($locale = 'en')
     {
         return sprintf('adminer-%s-%s.php', $this->version, strtolower($locale));
+    }
+
+    /**
+     * Mapping laravel db connection to adminer driver
+     *
+     * @param $connection
+     * @return string
+     */
+    private function getDatabaseDriver($connection)
+    {
+        /*
+            Adminer driver options
+
+            <option value="server">MySQL</option>
+            <option value="sqlite">SQLite 3</option>
+            <option value="sqlite2">SQLite 2</option>
+            <option value="pgsql">PostgreSQL</option>
+            <option value="oracle">Oracle (beta)</option>
+            <option value="mssql" selected="">MS SQL (beta)</option>
+            <option value="firebird">Firebird (alpha)</option>
+            <option value="simpledb">SimpleDB</option>
+            <option value="mongo">MongoDB</option>
+            <option value="elastic">Elasticsearch (beta)</option>
+            <option value="clickhouse">ClickHouse (alpha)</option>
+        */
+
+        switch ($connection) {
+            case "mysql":
+                return "server";
+            case "sqlsrv":
+                return "mssql";
+            default:
+                if (is_null($connection)) {
+                    return "server";
+                }
+                return $connection;
+        }
     }
 }
